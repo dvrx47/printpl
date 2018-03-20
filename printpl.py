2c38dd25d5725c509c19280b8c3eee909c7a99fb
dvrx47 <dvrx47git@gmail.com>
2018-03-20 22:36:43 +0100

    Add first version of dictionary (working-around)

--- printpl.py
+++ printpl.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
 
 import sys
+import re
+
+decode_pl = { '¡' : 'ą',
+              '¢' : 'ć',
+              '¦' : 'ę',
+              'ª' : 'ł',
+              '«' : 'ń',
+              '±' : 'ś',
+              '»' : 'ż',
+              '¹' : 'ź',
 
 for line in sys.stdin:
-    print(line, end='')
+    line_l = list(line)
+
+    print( re.sub(' +', ' ',''.join( map(lambda x : x if x not in decode_pl else decode_pl[x], line_l))) , end='')
